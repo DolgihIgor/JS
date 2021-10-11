e29551483999695ba1ac761f48c9e360071a3c17
DolgihIgor <87515633+DolgihIgor@users.noreply.github.com>
2021-10-11 16:34:00 +0700

    Тест WS

---
+++ newproject/js.js
@@ -0,0 +1,34 @@
+"use strict"
+
+let number = 5;
+
+let mas = 78;
+
+console.log(-4/0);
+
+console.log(number);
+const a = 'Ivan';
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = true;
+if (bool) {
+    console.log("true");
+}
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+// console.log(obj.name);
+ console.log(obj["name"]);
+
+let und;
+console.log(und);
+
+
+// alert(5);
+// [].push('a');
